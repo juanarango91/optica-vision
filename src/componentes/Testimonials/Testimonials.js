0a3adbdebecbd8d504628e04929d9ee33fbd4e5b
JuanTobon26M <monusjuande@gmail.com>
2024-10-08 21:33:51 -0500

    se agregan estilos

--- src/componentes/Testimonials/Testimonials.js
+++ src/componentes/Testimonials/Testimonials.js
@@ -7,6 +7,8 @@ const Testimonials = () => {
             <h2>Testimonios</h2>
             <p>"Excelente atención y productos de calidad!" - Juan P.</p>
             <p>"Me encantan mis nuevas gafas, gracias Óptica Visión!" - Ana G.</p>
+            <p>"Me ayudaron a encontrar las gafas perfectas para mi cara, gracias. - Mariana L.</p>
+            <p>"Me ayudaron con la cita que necesitaba para mi mama, muchas graciaas!" - Sofia R.</p>
         </div>
     );
 };
