9b0b23a02c7125a49ebfae78ae818644d5e8f0ad
JuanTobon26M <monusjuande@gmail.com>
2024-10-08 23:19:37 -0500

    se agrega estilos

--- src/componentes/Products/Productos.js
+++ src/componentes/Products/Productos.js
@@ -4,12 +4,15 @@ import './Products.css';
 const Products = () => {
     return (
         <div>
-            <h2>Productos</h2>
+            <h2>Nuestros Productos</h2>
             <ul>
-                <li>Gafas de sol</li>
+                <li>Gafas Transitions</li>
                 <li>Gafas graduadas</li>
+                <li>Gafas de diseño </li>
                 <li>Lentes de contacto</li>
                 <li>Accesorios para lentes</li>
+                <li>Monturas a tu estilo</li>
+                <li>A tu formula</li>
             </ul>
         </div>
     );
